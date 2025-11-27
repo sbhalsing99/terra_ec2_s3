fac5260f7d1d4e4819783ae9eaf4af9b08885274
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-27 19:49:45 +0530

    commit 2

--- remote-infra/terraform.tf
+++ remote-infra/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "shruti-tf-backend-bucket"
-    key            = "main/terraform.tfstate"
-    region         = "ap-south-1"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "terraform-state-lock-table"
     encrypt        = true
   }
